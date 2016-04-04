a8474bc6c05aed49b2be02b4da43472a9bbf17a6
Danayal Iftikhar <40171342@live.napier.ac.uk>
2016-04-04 22:27:31 +0100

    Added Messages class

--- NoogleProject/NoogleProject/MessageFilter.cs
+++ NoogleProject/NoogleProject/MessageFilter.cs
@@ -9,69 +9,6 @@ namespace NoogleProject
 {
     public class MessageFilter
     {
-        private string name;
-        private string email;
-        private string message;
-
-
-        public string Name
-        {
-            get
-            {
-                return name;
-            }
-            set
-            {
-                if (name == null)
-                {
-                    throw new Exception("Please enter a Name");
-                }
-                else
-                {
-                    value = name;
-                }
-            }
-        }
-
-
-        public string Email
-        {
-            get
-            {
-                return email;
-            }
-            set
-            {
-                if (email == null)
-                {
-                    throw new Exception("Please enter an email address");
-                }
-                else
-                {
-                    value = email;
-                }
-            }
-        }
-
-        public string Message
-        {
-            get
-            {
-                return message;
-            }
-            set
-            {
-                if (message == null)
-                {
-                    throw new Exception("Please enter a message");
-                }
-                else
-                {
-                    value = message;
-                }
-            }
-        }
-
         public void Reader(string message)
         {
             try
